e700c46684f360f7b83d2fc5981924c8400fce92
AlexanderRequiem <96475139+AlexanderRequiem@users.noreply.github.com>
2022-10-15 08:07:02 +0600

    edit api

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,36 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    let formData = null;
 
+    let url = options.url;
+    if (options.data) {
+        if (options.method === 'GET') {
+            url += '?' + Object.entries(data).map(
+                entry => entry.map(encodeURIComponent).join('=')
+            ).join('&');
+        } else {
+            formData = new FormData();
+            Object.entries(data).forEach(v => formData.append(...v));
+        }
+    }
+
+    xhr.onload = () => {
+        let err = null;
+        let resp = null;
+        try {
+            if (xhr.response?.success) {
+                resp = xhr.response;
+            } else {
+                err = xhr.response;
+            }
+        }  catch(e) {
+            err = e;
+        }
+        options.callback(err, resp);
+    }
+    xhr.open(options.method, url);
+    xhr.send(formData);
 };
